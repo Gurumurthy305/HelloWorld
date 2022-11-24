f7718ccecab67c153160ab989b800a1314aef151
Gurumurthy <gurumurthy.ravi@benjaminmoore.com>
2022-11-24 09:15:22 +0530

    added HelloWorld Program

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class hello {
+   public static void main(String[] args) {
+        for (var i = 1; i <=10; i++) {
+          System.err.println("Hello World... " + i );
+            
+        }
+    
+        
+    }
+}
